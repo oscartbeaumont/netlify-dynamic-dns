21b0dfe1e6de6b97817f6994a125e0f890613b89
Amir H. Sadoughi <amirhomayoun@users.noreply.github.com>
2019-07-05 02:38:43 -0400

    Delete existing record only if it has bad values
    Previously all the values would get deleted. With this change, the record will get deleted only if has the same name (subdomain), but the value (IP address) is incorrect.

--- pkg/recordUpdate.go
+++ pkg/recordUpdate.go
@@ -3,11 +3,12 @@ package pkg
 // UpdateRecord will find all current records and removes them if they are not valid. If no valid records are found it will create a new one.
 func UpdateRecord(domain string, accessToken string, record DNSRecord, records []DNSRecord) error {
 	correctRecordExists := false
+        var newName = record.Name+"."+domain
 	for _, r := range records {
 		if r.Type == record.Type {
-			if r.Name == record.Name+"."+domain && r.Value == record.Value && !correctRecordExists {
+			if r.Name == newName && r.Value == record.Value {
 				correctRecordExists = true
-			} else {
+			} else if r.Name == newName {
 				DeleteRecord(domain, accessToken, r)
 			}
 		}
