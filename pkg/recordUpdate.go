07d1f5ce1d492f79f87f97d60934e6dc8dded754
Amir H. Sadoughi <amirhomayoun@users.noreply.github.com>
2019-07-05 02:54:11 -0400

    Cosmetic change for gofmt

--- pkg/recordUpdate.go
+++ pkg/recordUpdate.go
@@ -3,7 +3,7 @@ package pkg
 // UpdateRecord will find all current records and removes them if they are not valid. If no valid records are found it will create a new one.
 func UpdateRecord(domain string, accessToken string, record DNSRecord, records []DNSRecord) error {
 	correctRecordExists := false
-        var newName = record.Name+"."+domain
+        var newName = record.Name + "." + domain
 	for _, r := range records {
 		if r.Type == record.Type {
 			if r.Name == newName && r.Value == record.Value {
