06a91f209e04285563811826e8dfd9b13ba7437e
oreoluwa <oreoluwa@outlook.com>
2019-09-18 14:55:22 -0400

    [fix]: scope record deletion/replacement to subdomain being updated

--- pkg/recordUpdate.go
+++ pkg/recordUpdate.go
@@ -1,11 +1,15 @@
 package pkg
 
+import (
+	"strings"
+)
+
 // UpdateRecord will find all current records and removes them if they are not valid. If no valid records are found it will create a new one.
 func UpdateRecord(domain string, accessToken string, record DNSRecord, records []DNSRecord) error {
 	correctRecordExists := false
 	for _, r := range records {
-		if r.Type == record.Type && r.Name == record.Name {
-			if r.Value == record.Value+"."+domain {
+		if r.Type == record.Type && strings.HasPrefix(r.Name, record.Name) {
+			if r.Value == record.Value {
 				correctRecordExists = true
 			} else {
 				DeleteRecord(domain, accessToken, r)
