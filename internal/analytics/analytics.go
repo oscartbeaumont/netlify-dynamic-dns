5a1115244085ebcdbbfb9dbf85c75f555885fc5c
junyu33 <2658799217@qq.com>
2024-11-18 16:09:29 +0800

    fix: removed analytics server

--- internal/analytics/analytics.go
+++ internal/analytics/analytics.go
@@ -1,12 +1,7 @@
 package analytics
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"os"
-	"runtime"
 	"time"
 )
 
@@ -18,46 +13,46 @@ var client = &http.Client{
 
 // Report an analytics event. The event is sent to a custom proxy for Simple Analytics which adds the users location based on their public IP because this is hard to do in native Go.
 func Report(version string) error {
-	if os.Getenv("NDDNS_DISABLE_ANALYTICS") == "true" {
-		return nil
-	}
+   /* if os.Getenv("NDDNS_DISABLE_ANALYTICS") == "true" {*/
+		/*return nil*/
+	/*}*/
 
-	// Note: The OS versions are not updated as the only purpose of request not being reported as Bot and containing correct OS
-	var userAgent = "Mozilla/5.0 "
-	if runtime.GOOS == "windows" {
-		userAgent += "(Windows NT 10.0; Win64; x64)"
-	} else if runtime.GOOS == "darwin" {
-		userAgent += "(Macintosh; Intel Mac OS X 11_1)"
-	} else if runtime.GOOS == "linux" {
-		userAgent += "(X11; Linux x86_64)"
-	}
+	/*// Note: The OS versions are not updated as the only purpose of request not being reported as Bot and containing correct OS*/
+	/*var userAgent = "Mozilla/5.0 "*/
+	/*if runtime.GOOS == "windows" {*/
+		/*userAgent += "(Windows NT 10.0; Win64; x64)"*/
+	/*} else if runtime.GOOS == "darwin" {*/
+		/*userAgent += "(Macintosh; Intel Mac OS X 11_1)"*/
+	/*} else if runtime.GOOS == "linux" {*/
+		/*userAgent += "(X11; Linux x86_64)"*/
+	/*}*/
 
-	var body = map[string]interface{}{
-		"name":     "pageview",
-		"url":      "https://nddns.app.otbeaumont.me/" + version,
-		"domain":   "nddns.app.otbeaumont.me",
-		"referrer": runtime.GOOS,
-	}
+	/*var body = map[string]interface{}{*/
+		/*"name":     "pageview",*/
+		/*"url":      "https://nddns.app.otbeaumont.me/" + version,*/
+		/*"domain":   "nddns.app.otbeaumont.me",*/
+		/*"referrer": runtime.GOOS,*/
+	/*}*/
 
-	eventJSON, err := json.Marshal(body)
-	if err != nil {
-		return fmt.Errorf("error marshaling analytics event: %w", err)
-	}
+	/*eventJSON, err := json.Marshal(body)*/
+	/*if err != nil {*/
+		/*return fmt.Errorf("error marshaling analytics event: %w", err)*/
+	/*}*/
 
-	req, err := http.NewRequest("POST", analyticsReportEndpoint, bytes.NewBuffer(eventJSON))
-	if err != nil {
-		return fmt.Errorf("error creating analytics event request: %w", err)
-	}
+	/*req, err := http.NewRequest("POST", analyticsReportEndpoint, bytes.NewBuffer(eventJSON))*/
+	/*if err != nil {*/
+		/*return fmt.Errorf("error creating analytics event request: %w", err)*/
+	/*}*/
 
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("User-Agent", userAgent)
+	/*req.Header.Set("Content-Type", "application/json")*/
+	/*req.Header.Set("User-Agent", userAgent)*/
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("error posting analytics event: %w", err)
-	} else if resp.StatusCode != http.StatusAccepted {
-		return fmt.Errorf("error reported status %v with analytics event", resp.StatusCode)
-	}
+	/*resp, err := client.Do(req)*/
+	/*if err != nil {*/
+		/*return fmt.Errorf("error posting analytics event: %w", err)*/
+	/*} else if resp.StatusCode != http.StatusAccepted {*/
+		/*return fmt.Errorf("error reported status %v with analytics event", resp.StatusCode)*/
+	/*}*/
 
 	return nil
 }
